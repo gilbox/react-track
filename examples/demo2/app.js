eda2bc50756c0491716f54186d6fa7869448a46a
gilbox <birmangil@gmail.com>
2015-08-29 21:07:49 -0700

    more gaudy animation

--- examples/demo2/app.js
+++ examples/demo2/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {Easer} from 'functional-easing';
 import {tween} from 'react-track/tween';
-import {rotate} from 'react-track/tween-value-factories';
+import {rotate, percent} from 'react-track/tween-value-factories';
 import Scrubber from './Scrubber';
 import Timeline from 'react-track/timeline';
+import {Spring} from 'react-motion';
 
 const easeOutBounce = new Easer().using('out-bounce');
+const easeOutSine = new Easer().using('out-sine');
 
 const MIN_TIME = 0;
 const MAX_TIME = 100;
@@ -14,6 +16,14 @@ const styles = {
   scrubber: {
     width: '100%',
     boxSizing: 'border-box',
+  },
+  ball: {
+    borderRadius: '50%', 
+    width: '20px', 
+    height: '20px',
+    position: 'absolute',
+    backgroundColor: 'white',
+    opacity: 0.2
   }
 };
 
@@ -26,10 +36,33 @@ class App extends Component {
         min={MIN_TIME} 
         max={MAX_TIME} 
         loop={true}>
-      {({time, playing, togglePlay, setTime}) =>
+      {({time, playing, togglePlay, setTime}) => {
         
-        <div style={{padding: 30}}>
+        const top = (100 + 40 * Math.sin(time/5));
+        const left = tween(time, {
+          [MIN_TIME]: 0,
+          [MAX_TIME]: 100
+        }, easeOutSine);
         
+        return <div style={{padding: 30}}>
+        
+          <Spring endValue={{val: {top,left}}}>
+          {interpolated =>
+            <div
+              style={{
+                ...styles.ball,
+                top: interpolated.val.top,
+                left: interpolated.val.left + '%',
+              }} />
+          }</Spring>
+          
+          <div 
+            style={{
+              ...styles.ball,
+              top,
+              left: left + '%'
+            }}/>
+          
           <h2
             style={tween(time, {
               [MIN_TIME]: { transform: rotate(0) },
@@ -49,7 +82,7 @@ class App extends Component {
             value={time}
             onChangeValue={setTime} />
             
-        </div>
+        </div>}
         
       }</Timeline>
     )
